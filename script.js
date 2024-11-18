656b05d85841052f6b2ce5dcc00a9de864e0ded1
Roshan632 <rosh45644@gmail.com>
2024-11-18 20:54:20 +0545

    First commit

---
+++ script.js
@@ -0,0 +1,23 @@
+var bulb = document.querySelector("#bulb")
+var btn = document.querySelector("button")
+var flag = 0
+btn.addEventListener("click",function(){
+     if(flag==0){
+        
+        btn.style.backgroundcolor="Yellow"
+        console.log("Clicked")
+         flag=1
+        
+     }else{
+        bulb.style.backgroundcolor="transparent"
+       console.log("Again Clicked")
+         flag=0
+        
+
+     }
+ })  ; 
+
+
+    
+    
+
